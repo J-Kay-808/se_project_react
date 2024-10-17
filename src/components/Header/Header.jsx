ad857b87bc662151ab2e16ea40156093cad43583
= <=>
2024-10-16 19:09:02 -0500

    refactored

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ function Header({
   return (
     <header className="header">
       <div className="header__logo-and-date">
-        <Link to="/profile">
+        <Link to="/">
           <img className="header__logo" src={headerLogo} alt="Header Logo" />
         </Link>
         <p className="header__date-and-location">
